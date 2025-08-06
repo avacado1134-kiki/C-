6c1d6484d02131268139fa9a2a5e86d825d05fce
Aakriti <avacado1134@gmail.com>
2025-08-07 00:33:17 +0530

    bugs fixed

--- C Language/Chapter 4 loop/06_for.c
+++ C Language/Chapter 4 loop/06_for.c
@@ -6,7 +6,7 @@ int main()
     scanf("%d", &n);
     for (int i = 1; i <= n; i++)// initialise;test;increment/decrement
     {                                       
-        printf("enter value if i %d\n", i); // i.e condition check hogi true hai toh block execute hogi then i update hogi
+        printf("enter value of i %d\n", i); // i.e condition check hogi true hai toh block execute hogi then i update hogi
     }
     return 0;
 }
--- C Language/Chapter 4 loop/07_decrementing_forloop.c
+++ C Language/Chapter 4 loop/07_decrementing_forloop.c
@@ -2,10 +2,15 @@
 
 int main()
 {
-   
-    for (int i = 5; i; i--)//i ki value jab tak non zero hai condition is true
+
+    for (int i = 5; i; i--) // i ki value jab tak non zero hai condition is true
+    // int i = 5; → Initialization: start with i = 5
+
+    // i; → Condition: while i is non-zero (true), continue
+
+    // i-- → Update: decrement i after every loop
     {
-        printf("me %d\n", i); // this for loop will continue running until i becomes 0
+        printf("me %d\n", i); // this (for loop) will continue running until i becomes 0
     }
     return 0;
 }
