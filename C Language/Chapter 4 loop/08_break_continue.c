65fde078a1852f825938bfe39b09b45286b118b3
Aakriti <avacado1134@gmail.com>
2025-08-07 00:39:42 +0530

    comment out

--- C Language/Chapter 4 loop/08_break_continue.c
+++ C Language/Chapter 4 loop/08_break_continue.c
@@ -1,5 +1,6 @@
-// any loop terminates by break
-//iteration skips by continue
+
+// break : any loop terminates by break
+//continue : iteration skips by continue
 #include <stdio.h>
 
 int main()
@@ -7,11 +8,16 @@ int main()
     for (int i = 0; i < 15; i++)
     {
         if(i==5){
-            //break;//exit the loop i.e terminates loop
+            // break;//exit the loop i.e terminates loop
             continue;//exit this iteration just for now i.e iteration terminates
         }
         printf("i is %d\n", i);
-    }//output 0-4
+    }
+    //WHEN USED BREAK
+    //output 0-4
     //output 5 will not get printed
+
+    //WHEN USED CONTINUE
+    //output 0-14,but will not print 5
     return 0;
 }
