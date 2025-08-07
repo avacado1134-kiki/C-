09f216767dd752180199496fead65e49be5dae2e
Aakriti <avacado1134@gmail.com>
2025-08-07 22:48:35 +0530

    formality

--- C Language/DSA/ch 4 practise set/01_prob.c
+++ C Language/DSA/ch 4 practise set/01_prob.c
@@ -1,15 +1,30 @@
-//write a program to print multiplication table pf a given no. n
+// write a program to print multiplication table pf a given no. n
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n, i;
+//     scanf("%d", &n);
+//     for (i = 1; i < 11; i++)
+//     {
+//         printf("%d x %d=%d\n", n, i, n * i);
+//     }
+
+//     return 0;
+// }
 
 #include <stdio.h>
 
 int main()
 {
-    int n, i;
-    scanf("%d", &n);
-    for (i = 1; i < 11; i++)
+    int n, mul;
+    printf("enter number\n");
+    scanf("%d",&n);
+    for (int i = 1; i <= 10; i++)
     {
-        printf("%d x %d=%d\n", n, i, n * i);
+        mul = n * i;
+        printf("%d x %d=%d\n", n, i, mul);
     }
-
     return 0;
 }
